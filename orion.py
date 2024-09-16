708cd53915a49c75a7cf393d2285f9a337192033
Toufik Saouchi <saouchi.toufik@gmail.com>
2024-09-16 15:54:38 +0200

    example for testing filter

--- orion.py
+++ orion.py
@@ -45,33 +45,32 @@ if __name__ == "__main__":
     # Create a simple sine wave with noise
     sampling_rate = 1000  # Hz
     t = np.linspace(0, 1, sampling_rate)
-    input_signal = np.sin(2 * np.pi * 5 * t)  # 5 Hz sine wave
-    input_signal += 0.5 * np.random.normal(size=t.shape)
+    input_signal1 = np.sin(2 * np.pi * 20 * t)  # 5 Hz sine wave
+    input_signal = input_signal1 + np.sin(2 * np.pi * 5 * t)
 
     base = Orion.Base()
     base.init()
     base[0][0].add_variable('TimeValue', t)
     base[0][0].add_variable('input_signal', input_signal)
+    base[0][0].add_variable('input_signal1', input_signal1)
     
     filter_config = {
-        'cutoff' : 6,
-        'btype' : 'high',
+        'cutoff' : (1, 30),
+        'btype' : 'stop',
         'order' : 1,
         'sampling_rate' : sampling_rate
     }
-    from copy import deepcopy  # Import deepcopy to copy the original data
-
-    base_filter = Processor(deepcopy(base)).filter(**filter_config)
+    base_filter = Processor(base).filter(**filter_config)
     
     plot_dictionary = {
         'Time (s) - Shifted to start at zero' : {
-            'values' : [base[0][0][0].data, base_filter[0][0][0].data],
-            'markers' : 2*['lines'],
-            'legend' : ["original", "filtered"],
-            'sizes' : 2*[2]
+            'values' : [base[0][0][0].data, base_filter[0][0][0].data, base_filter[0][0][0].data],
+            'markers' : 3*['lines'],
+            'legend' : ["original", "filtered", "input"],
+            'sizes' : 3*[2]
         },
         'Displacement/BMW maximum dispalcement' : {
-            'values' : [base[0][0][1].data, base_filter[0][0][1].data],
+            'values' : [base[0][0][1].data, base_filter[0][0][1].data,base[0][0][2].data],
         },
     }
     output_path = r"C:\__sandBox__\Data\13124-846_G60 BMW DVP - Pink noise signal"
